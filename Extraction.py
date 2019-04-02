bbfaca24dd7fea9c922297d604d7810cfe571bf1
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-04-02 16:32:07 -0600

    Successfully Coded Repetition in Case the isn't access to DBs

--- Extraction.py
+++ Extraction.py
@@ -1,23 +1,33 @@
 import os
 from os import system as cmd
+from sys import exit
 
 server = '//10.109.10.140/InventariosCC'
 
-### 0.- ERROR HANDLER
-try:
-    ls_qvd = os.listdir(server)
-    print(server+'... Ready')
-except:
-    input('Server not in line...')
-    exit()
-
 ### 1.- QLIKVIEW WITH COMMAND LINE
-print('\nExtracting...')
+def Extraer():
 
-qvdExe = 'C:/"Program Files"/QlikView/qv.exe'
-qvdQVW = ' /r "C:/Users/alexisalvarez/OneDrive - Grupo Vidanta/UPDATE/Work/00. QVDs/Extraction/DataExtraction/'
-qvdQVW = qvdQVW+'Extraction.qvw"'
+    print('\nExtracting...')
 
-cmd(qvdExe+qvdQVW)
+    qvdExe = 'C:/"Program Files"/QlikView/qv.exe'
+    qvdQVW = ' /r "C:/Users/alexisalvarez/OneDrive - Grupo Vidanta/UPDATE/Work/00. QVDs/Extraction/DataExtraction/'
+    qvdQVW = qvdQVW+'Extraction.qvw"'
 
-print('\nFinished...')
+    cmd(qvdExe+qvdQVW)
+
+    print('\nFinished...')
+    os._exit(1)
+
+### 0.- ERROR HANDLER
+def Loop():
+    try:
+        os.listdir(server)
+        Extraer()
+    except:
+        inp = input('\nUpdate? (anykey == yes/no)...')
+        if inp == 'no': 
+            exit()
+        while True:
+            Loop()
+        exit()
+Loop()
